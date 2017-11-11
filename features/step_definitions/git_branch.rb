2e0e8ea466e0362a43254e7a4c34384553a5c4f7
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2017-11-11 23:02:56 +0530

    I did not add the files earlier, so adding all the files and then commiting it for staging

---
+++ features/step_definitions/git_branch.rb
@@ -0,0 +1,11 @@
+Given(/^I create a new branch with the command "git branch (.*) \(ph\)$/) do |my_first_branch|
+  pending
+end
+
+And(/^I move to the branch using "git checkout (.*) \(ph\)$/) do |your_branch_name|
+  pending
+end
+
+Then(/^I should be able to move to the newly created branch and work there\. \(ph\)$/) do
+  pending
+end
